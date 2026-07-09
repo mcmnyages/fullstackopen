952f5d7ce70ed4b2d9dce9cf97ef9c7815544601
McMnyages <morachasilas@gmail.com>
2026-07-09 11:23:12 +0300

    refactor: update Button and Statistics components to handle props and state correctly

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,30 +1,51 @@
+import { useState } from "react"
+
+
 
 const Button = (props) =>{
+  // console.log('Props in button',props)
   return(
-    <button>{props.text}</button>
+    <button onClick={props.onClick} >{props.text}</button>
   )
 }
 
-const Statistics =()=>{
+const Statistics =(props)=>{
+  // console.log("Props in stats",props)
   return(
     <div>
       <h1>Statistics</h1>
-      <p>Good:6</p>
-      <p>Neutral:2</p>
-      <p>Bad:1</p>
+      <p>Good:{props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
     </div>
   )
 }
 
 
 const App =() => {
+
+const [good,setGood]=useState(0)
+const [neutral,setNeutral]=useState(0)
+const [bad,setBad]=useState(0)
+
+const handleGoodCount=()=>{
+  return setGood(good+1)
+}
+
+const handleNeutralCount=()=>{
+  return setNeutral(neutral+1)
+}
+
+const handleBadCount=()=>{
+  return setBad(bad+1)
+}
   return(
     <div>
       <h1>Give feedback</h1>
-      <Button text={'good'}/>
-      <Button text={'neutral'}/>
-      <Button text={'bad'}/>
-      <Statistics/>
+      <Button onClick={handleGoodCount } text={'good'} count={good}/>
+      <Button onClick={handleNeutralCount} text={'neutral'} count={neutral}/>
+      <Button onClick={handleBadCount} text={'bad'} count={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
