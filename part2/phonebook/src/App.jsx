bfb6c5a79e0d45bcaf2810e7885ae1c80640a378
McMnyages <morachasilas@gmail.com>
2026-07-28 21:46:56 +0300

    feat: implement form submission for adding new persons to the phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,23 +2,47 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' },
   ]) 
   const [newName, setNewName] = useState('')
+
+  const addPerson =(e)=>{
+    e.preventDefault()
+    const  personObj={
+      name:newName
+     }
+  setPersons(persons.concat(personObj))
+  setNewName('')
+  }
+
+const handlePersonChange =(e)=>{
+  setNewName(e.target.value)
+}
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName} 
+          onChange={handlePersonChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {
+          persons.map(person=>(
+            <li key={person.name}>
+              {person.name}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
