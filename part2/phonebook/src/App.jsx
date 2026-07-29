fd7a8fb88b19e8e7d917a8a5de6cba3e1167af68
McMnyages <morachasilas@gmail.com>
2026-07-29 09:07:48 +0300

    feat: add search functionality to filter displayed contacts in phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040 1234567',
-    },
+   const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [number, setNumber] = useState('')
+  const [search, setSearch]= useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
@@ -36,11 +37,20 @@ const App = () => {
   const handleNumberChange = (e) => {
     setNumber(e.target.value)
   }
+  const handleSearch =(e)=>{
+    setSearch(e.target.value)
+  }
+  const personsToShow= persons.filter(person=>person.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
-
+      filter shown with
+      <input 
+      value={search}
+      onChange={handleSearch}
+      />
+      <h1>Add New</h1>
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -66,7 +76,7 @@ const App = () => {
       <h2>Numbers</h2>
 
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
           </li>
