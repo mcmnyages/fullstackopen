b46675f3e653edfcb992be9b49b1d36a4886d5fc
McMnyages <morachasilas@gmail.com>
2026-08-03 11:52:12 +0300

    Add package.json and initial phone request example
    - Created package.json for phonebook_backend with dependencies and scripts.
    - Added a sample phone request in phone_requests.rest for creating a new person.

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,88 @@
+import express, { json, response } from 'express'
+
+const app= express()
+
+app.use(json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/',(request,response)=>{
+    response.send('<h1>It is working finally</h1>')
+})
+
+app.get('/api/info',(request,response)=>{
+
+    response.send(`<p>Phonebook has infor for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
+})
+
+app.get('/api/persons',(request,response)=>{
+    response.json(persons)
+})
+
+app.get('/api/persons/:id',(request,response)=>{
+    const id = request.params.id
+    const person = persons.find(person=>person.id===id)
+
+    if(id){
+        response.json(person)
+    }else {
+    response.status(404).end()
+  }
+
+})
+
+
+app.post('/api/persons',(request,response)=>{
+  const body = request.body
+
+  const person ={
+    id:Math.floor(Math.random()* 10000000),
+    name:body.name,
+    number:body.number
+  }
+
+  persons = persons.concat(person);
+
+    response.json(persons)
+})
+
+app.delete('/api/persons/:id',(request, response)=>{
+   const id = request.params.id
+    const person = persons.filter(person=>person.id!==id)
+
+    if(id){
+        response.status(204).end
+    }else {
+    response.status(404).end()
+  }
+ 
+})
+
+const PORT=3001
+
+app.listen(PORT,()=>{
+    console.log(`Server UP and listening to port ${PORT}`)
+})
