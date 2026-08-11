cfd867352ef2540543115536a324baf37177a67a
McMnyages <morachasilas@gmail.com>
2026-08-11 13:03:34 +0300

    feat: format person schema for consistency and clarity

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -20,12 +20,23 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name:{
+  name: {
     type: String,
-    minLength:3,
-    required:true,
+    minLength: 3,
+    required: true,
   },
-  number: String,
+
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(value) {
+        return /^\d{2,3}-\d+$/.test(value)
+      },
+      message: props => `${props.value} is not a valid phone number`,
+    },
+  },
 })
 
 personSchema.set('toJSON', {
