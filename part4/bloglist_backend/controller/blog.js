7edd209c9d6f40dd10929158d7f53f3d16bd8a93
McMnyages <morachasilas@gmail.com>
2026-08-19 11:15:26 +0300

    feat: enhance blog and user routes; populate user in blog retrieval and blogs in user retrieval

--- part4/bloglist_backend/controller/blog.js
+++ part4/bloglist_backend/controller/blog.js
@@ -1,32 +1,40 @@
 const blogRouter = require('express').Router()
 const { response } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
-
 })
 
+// POST new blog
 blogRouter.post('/', async (request, response) => {
-  try {
-    const body = request.body
+  const body = request.body
 
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes
-    })
+  // Temporary: pick the first user from the database as the creator
+  const users = await User.find({})
+  const user = users[0]
 
-    const savedBlog = await blog.save()
-
-    response.status(201).json(savedBlog)
-  } catch (error) {
-    response.status(400).json({
-      error: error.message
-    })
+  if (!user) {
+    return response.status(400).json({ error: 'no users found in database, create a user first' })
   }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  
+  // Save the blog reference to the user's blogs array
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogRouter.put('/:id', async (request, response) => {
