74b223baed528334ec7bcbd06d7e0c25c80aeb34
McMnyages <morachasilas@gmail.com>
2026-08-24 19:30:33 +0300

    feat: refactor Blog component to manage visibility state and display additional blog details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (!visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(true)}>
+          view
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <div  style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setVisible(false)}>
+        hide
+      </button>
+
+      <div>{blog.url}</div>
+      <div>likes {blog.likes}</div>
+      <div>{blog.user?.name}</div>
+
+      <button>like</button>
+    </div>
+  )
+}
 
 export default Blog
