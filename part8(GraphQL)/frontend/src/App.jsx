fff2c83bbda0600c858b7de2237385817b2872ac
McMnyages <morachasilas@gmail.com>
2026-09-18 12:03:59 +0300

    feat: implement Apollo Client setup and add person management features

--- part8(GraphQL)/frontend/src/App.jsx
+++ part8(GraphQL)/frontend/src/App.jsx
@@ -1,19 +1,31 @@
-import { useEffect,useState } from "react";
+import { useQuery } from '@apollo/client/react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import { ALL_PERSONS } from './queries'
 
-const App =()=>{
-  const [ time, setTime] = useState(new Date())
-  useEffect(()=>{
-    const timerID = setInterval(()=>{
-      setTime(new Date())
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+  const [time, setTime] = useState(new Date)
+  useEffect(() => {
+    const timeId = setInterval(() => {
+      setTime(new Date)
     }, 1000)
-    console.log('Timer id', timerID)
-    return ()=>{
-      clearInterval(timerID)
-    }
-  },[])
-  return(
+    return () => clearInterval(timeId)
+  })
+
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
     <div>
-      <h1>Time {time.toLocaleTimeString()}</h1>
+      <h1>{time.toLocaleTimeString()}</h1>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm />
     </div>
   )
 }
