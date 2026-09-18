5ff083bf0cc6d029868a0dd1e3781031f239d3a0
McMnyages <morachasilas@gmail.com>
2026-09-18 12:44:32 +0300

    feat: add phone number management with PhoneForm component and error notification

--- part8(GraphQL)/frontend/src/App.jsx
+++ part8(GraphQL)/frontend/src/App.jsx
@@ -4,11 +4,21 @@ import { useState } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
-
+import Notify from './components/Notify'
+import PhoneForm from './components/PhoneForm'
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
   const [time, setTime] = useState(new Date)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   useEffect(() => {
     const timeId = setInterval(() => {
       setTime(new Date)
@@ -24,8 +34,10 @@ const App = () => {
   return (
     <div>
       <h1>{time.toLocaleTimeString()}</h1>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   )
 }
